5390347edc107809b5ef21ab54aada946a934cfd
Etobeliac <celestin.clbt@gmail.com>
2024-10-21 22:53:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,12 @@ else:
         exec(f.read())
 # Barre latérale pour la navigation
 options = ['G News'] + [f for f in os.listdir('scripts') if f.endswith('.py')]
-selected_option = st.sidebar.selectbox('', options)
+selected_option = st.sidebar.selectbox('G-News', options)
+
+# Affichage en fonction de l'option sélectionnée
+if selected_option == 'Accueil':
+    st.write('Utilisez la barre latérale pour accéder aux différents outils.')
+else:
+    script_path = os.path.join('scripts', selected_option)
+    with open(script_path) as f:
+        exec(f.read())
