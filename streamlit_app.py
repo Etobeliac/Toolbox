557a0958943b732f0c3c21bf70545482a52854d1
Etobeliac <celestin.clbt@gmail.com>
2024-10-21 22:43:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import os
 
 # Titre de l'application
-st.title('Bienvenue dans Toolbox')
+st.title('Mon Toolbox')
 
-# Instructions
-st.write('Utilisez la barre latérale pour sélectionner des outils.')
+# Barre latérale pour la navigation
+options = ['Accueil'] + [f for f in os.listdir('scripts') if f.endswith('.py')]
+selected_option = st.sidebar.selectbox('Choisissez un outil', options)
+
+# Affichage en fonction de l'option sélectionnée
+if selected_option == 'Accueil':
+    st.write('Utilisez la barre latérale pour accéder aux différents outils.')
+else:
+    script_path = os.path.join('scripts', selected_option)
+    with open(script_path) as f:
+        exec(f.read())
