ef3ddc22cfe5b3850a57ac653f0b94df29f9daf0
Etobeliac <celestin.clbt@gmail.com>
2024-10-21 22:57:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,23 +6,15 @@ st.title('Mon Toolbox')
 
 # Barre latérale pour la navigation
 options = ['Accueil'] + [f for f in os.listdir('scripts') if f.endswith('.py')]
-selected_option = st.sidebar.selectbox('', options)
+selected_option = st.sidebar.selectbox('Choisissez un outil', options)
 
 # Affichage en fonction de l'option sélectionnée
 if selected_option == 'Accueil':
     st.write('Utilisez la barre latérale pour accéder aux différents outils.')
 else:
     script_path = os.path.join('scripts', selected_option)
-    with open(script_path) as f:
-        exec(f.read())
-# Barre latérale pour la navigation
-options = ['G News'] + [f for f in os.listdir('scripts') if f.endswith('.py')]
-selected_option = st.sidebar.selectbox('G-News', options)
-
-# Affichage en fonction de l'option sélectionnée
-if selected_option == 'Accueil':
-    st.write('Utilisez la barre latérale pour accéder aux différents outils.')
-else:
-    script_path = os.path.join('scripts', selected_option)
-    with open(script_path) as f:
-        exec(f.read())
+    if os.path.exists(script_path):
+        with open(script_path) as f:
+            exec(f.read())
+    else:
+        st.error(f"Le fichier {selected_option} n'existe pas.")
