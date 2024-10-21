a3114f28e05625f40af0d804b7a1373d2a564273
Etobeliac <celestin.clbt@gmail.com>
2024-10-21 23:07:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import os
+import importlib.util
 
 # Titre de l'application
 st.title('Mon Application avec Dérouleurs')
@@ -7,7 +9,8 @@ st.title('Mon Application avec Dérouleurs')
 st.sidebar.header('Menu')
 
 # Dérouleur 1
-option1 = st.sidebar.selectbox('G-News', ['Fichier 1', 'Fichier 2', 'Fichier 3'])
+option1 = st.sidebar.selectbox('G-News', ['Fichier 1', 'GPT Bulk', 'Fichier 3'])
+
 if option1 == 'Fichier 1':
     st.write('Contenu du Fichier 1')
 elif option1 == 'GPT Bulk':
@@ -19,21 +22,3 @@ elif option1 == 'GPT Bulk':
     gpt_bulk.run()  # Assurez-vous que gpt-bulk.py a une fonction run()
 else:
     st.write('Contenu du Fichier 3')
-
-# Dérouleur 2
-option2 = st.sidebar.selectbox('Choisissez une option 2:', ['Fichier A', 'Fichier B', 'Fichier C'])
-if option2 == 'Fichier A':
-    st.write('Contenu du Fichier A')
-elif option2 == 'Fichier B':
-    st.write('Contenu du Fichier B')
-else:
-    st.write('Contenu du Fichier C')
-
-# Dérouleur 3
-option3 = st.sidebar.selectbox('Choisissez une option 3:', ['Fichier X', 'Fichier Y', 'Fichier Z'])
-if option3 == 'Fichier X':
-    st.write('Contenu du Fichier X')
-elif option3 == 'Fichier Y':
-    st.write('Contenu du Fichier Y')
-else:
-    st.write('Contenu du Fichier Z')
